ec68e49ca7c42f9a12f95d097c3fab697e7b9f00
Stephen Neal <stephen.neal14@gmail.com>
2021-02-08 13:26:25 -0500

    MAKE ExtractTest for testing Extract methods
    FIX return type hinting in Extract::zip() method.

---
+++ tests/ExtractTest.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Sfneal\GooglePlaces\Tests;
+
+
+use Sfneal\GooglePlaces\Extract;
+
+class ExtractTest extends TestCase
+{
+    /**
+     * @var string Place description
+     */
+    public $description = 'Franklin, MA 02038';
+
+    /** @test */
+    public function cityState()
+    {
+        list($city, $state) = Extract::cityState($this->description);
+
+        $this->assertEquals('Franklin', $city);
+        $this->assertEquals('MA', $state);
+    }
+
+    /** @test */
+    public function zip()
+    {
+        $zip = Extract::zip($this->description);
+
+        $this->assertEquals('02038', $zip);
+    }
+
+    /** @test */
+    public function city()
+    {
+        $city = Extract::city($this->description);
+
+        $this->assertEquals('Franklin', $city);
+    }
+
+    /** @test */
+    public function state()
+    {
+        $state = Extract::state($this->description);
+
+        $this->assertEquals('MA', $state);
+    }
+}

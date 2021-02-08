5767f50ddea7afbbdce3a12475484a03e8f19038
Stephen Neal <stephen.neal14@gmail.com>
2021-02-08 11:15:22 -0500

    MAKE TestCase & update composer dev requirements

---
+++ tests/TestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sfneal\GooglePlaces\Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Clean up the testing environment before the next test.
+     *
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}

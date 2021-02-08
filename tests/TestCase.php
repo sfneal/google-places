42f79c51553d8588c7aa68d2681987dfa0d699f4
Stephen Neal <stephen.neal14@gmail.com>
2021-02-08 11:46:17 -0500

    MAKE ServiceProvider for publishing config & route files

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,25 @@
 
 namespace Sfneal\GooglePlaces\Tests;
 
+use Illuminate\Foundation\Application;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Sfneal\GooglePlaces\Providers\GooglePlacesServiceProvider;
 
 class TestCase extends OrchestraTestCase
 {
+    /**
+     * Register package service providers.
+     *
+     * @param Application $app
+     * @return array|string
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            GooglePlacesServiceProvider::class
+        ];
+    }
+
     /**
      * Setup the test environment.
      *
